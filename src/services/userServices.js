56f63041c5913ba293d77cbc0c5b25aa9571b9e2
mahisurrahman <zordanblog009@gmail.com>
2025-10-05 12:52:22 +0600

    Initiating

--- src/services/userServices.js
+++
@@ -1,6 +0,0 @@
-import apiClient from "../api/apiClient";
-
-export const getUsers = async () => {
-  const res = await apiClient.get("/users");
-  return res.data;
-};
--- src/utils/handleError.js
+++
@@ -1,9 +0,0 @@
-export default function handleError(error) {
-  if (error.response) {
-    return error.response.data?.message || "Server error.";
-  } else if (error.request) {
-    return "No response from server.";
-  } else {
-    return error.message || "Something went wrong.";
-  }
-}
